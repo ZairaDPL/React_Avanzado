e4072707809f6c7b537e7e62e1696706c7cb48bf
zaira_daniela_pacheco_limon <zairadpl18@gmail.com>
2025-07-10 21:28:22 -0600

    📝Importar components

--- 02.ReactContext/src/pages/Home.jsx
+++ 02.ReactContext/src/pages/Home.jsx
@@ -1,7 +1,12 @@
+import SongDetails from "../components/SongDetails/SongDetails";
+import SongList from "../components/SongList/SongList";
+
 const Home = () => {
     return (
         <>
-        <h1>bienvenido</h1>       
+            <h1>bienvenido</h1>      
+            <SongList />
+            <SongDetails />   
         </>
     )
 }
