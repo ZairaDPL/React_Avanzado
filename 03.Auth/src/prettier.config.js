bc277f20399210d77a10bee5ff5321102b8da3b1
zaira_daniela_pacheco_limon <zairadpl18@gmail.com>
2025-07-15 20:11:32 -0600

    ⚙ Configuración de prettier

---
+++ 03.Auth/src/prettier.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    singleQuote: false,
+    jsxSingleQuoute: false,
+    trailingComa: "es5",
+    semi: true, //agregar punto y coma al final de las lineas
+    arrowParens: "always"
+}
